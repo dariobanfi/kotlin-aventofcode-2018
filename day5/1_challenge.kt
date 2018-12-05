002e95d7a562ff5279468eb8004e23e0e9690f5f
Dario Banfi <dario.banfi9@gmail.com>
2018-12-05 20:31:05 +0100

    Day 5 first challenge

---
+++ day5/1_challenge.kt
@@ -0,0 +1,37 @@
+package day5
+
+import java.io.File
+
+fun main(args : Array<String>) {
+    val input = File("day5/input.txt").readText().toMutableList()
+
+    var stable = false
+    var collapsed = false
+    var i = 0
+    while (!stable) {
+        if (i + 2 > input.size) {
+            if (!collapsed) {
+                stable = true
+            }
+            i = 0
+            collapsed = false
+        } else {
+            if (doAtomsReact(input[i], input[i + 1])) {
+                input.removeAt(i)
+                input.removeAt(i)
+                collapsed = true
+            } else {
+                i++
+            }
+        }
+    }
+
+    println("Solution ${input.joinToString("").length}")
+}
+
+fun doAtomsReact(atom1: Char, atom2: Char): Boolean {
+    return when {
+        atom1.toLowerCase() == atom2.toLowerCase() && atom1 != atom2 -> true
+        else -> false
+    }
+}
